1373f633283a933c45602dd9c460a877db550d4d
Petya Kohts <petya@kohts.com>
2016-04-10 07:40:13 +0000

    https://rt.cpan.org/Ticket/Display.html?id=113640
    bumped version, rebuilt with newer ExtUtils::MakeMaker

--- lib/Schedule/Cron/Events.pm
+++ lib/Schedule/Cron/Events.pm
@@ -6,7 +6,7 @@ use Set::Crontab;
 use Time::Local;
 use vars qw($VERSION @monthlens);
 
-($VERSION) = ('$Revision: 1.94 $' =~ /([\d\.]+)/ );
+($VERSION) = ('$Revision: 1.95 $' =~ /([\d\.]+)/ );
 @monthlens = ( 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 );
 
 ## PUBLIC INTERFACE
